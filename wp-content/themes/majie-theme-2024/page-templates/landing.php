3083e58cc5a18694fa59576f50ffdf02273d0225
DaGuinci <daguinci@mailo.com>
2024-06-06 10:40:29 +0200

    landing(landing.php): setup content in progress

--- wp-content/themes/majie-theme-2024/page-templates/landing.php
+++ wp-content/themes/majie-theme-2024/page-templates/landing.php
@@ -1,30 +1,13 @@
 <?php /* Template Name: Page accueil */ ?>
 <?php
 get_header();
-$title = get_the_title();
+$news = get_field('news');
+var_dump($news);
 $content = get_the_content();
-$slider_pictures = get_field('home_slider');
 ?>
-<div class="article">
-  <div class="article__content">
-    <div class="article__content__text">
-      <div class="article__content__text__circles">
-        <?php for( $i = 0 ; $i < 12 ; $i++ ) { ?>
-          <svg class="circle" height="15"
-          viewBox="0 0 15 15"
-          xmlns="http://www.w3.org/2000/svg">
-            <circle id="blueLittleCircle" cx="7" cy="7" r="7" fill="#0A71B4" />
-          </svg>
-        <?php } ?>
-      </div>
-      <div class="article__content__text__content">
-        <h2 class="article__content__text__content__title">
-          <?= $title ?>
-        </h2>
-        <?= $content; ?>
-      </div>
-    </div>
+<section class="landing">
+  <div class="landing__who_are_we">
   </div>
-</div>
+</section>
 <?php
 get_footer();
